5d7cf0d2c1d7fce3b7a44cf61c501f77caff2826
ses9788 <ses9788@thi.de>
2020-11-13 16:38:09 +0100

    Aufgabe 1 fertig

---
+++ vrar-2020/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+    private Rigidbody rb = new Rigidbody();
+    public float speed;
+    public bool keyboard;
+    public static int score = -1;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {   if (keyboard == true)
+        {
+            float moveHoriontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHoriontal, 0f, moveVertical);
+
+            movement = movement * speed;
+
+            rb.AddForce(movement);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+        }
+    }
+
+}
---
+++ vrar-2020/Assets/Scripts/TextController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    private Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerController2.score == 10)
+        {
+            txt.text = "You're da man! \n Score: " + PlayerController2.score;
+        } else
+        {
+            txt.text = "Score: " + PlayerController2.score;
+        }
+    }
+}
