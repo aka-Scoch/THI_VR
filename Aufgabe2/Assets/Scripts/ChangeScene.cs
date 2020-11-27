a31f4bbc865a1fe9cf9123c56f6e1a3114b922ba
ses9788 <ses9788@thi.de>
2020-11-27 16:53:23 +0100

    Aufgabe 2 fertig

---
+++ Aufgabe2/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject canope;
+    public object scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.transform.rotation.y);
+        if (this.transform.rotation.y < -0.6)
+        {
+            //System.Threading.Thread.Sleep(1000);
+            SceneManager.LoadScene("cockpit");
+        }
+    }
+}
---
+++ Aufgabe2/Assets/Scripts/checkangle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkangle : MonoBehaviour
+{
+    public GameObject canope; 
+    float y_ref = 0;
+    float y_cur = 0;
+    float diff;
+    int i = 1;
+    bool open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y_ref = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y_cur = this.transform.position.y;
+        diff = y_cur - y_ref;
+        Debug.Log (diff);
+        Debug.Log (canope.transform.rotation.y);
+
+        if (diff > 0.05 && !open)
+        {
+            canope.transform.Rotate(0, -140, 0);
+            open = true;
+            
+        }
+    }
+}
